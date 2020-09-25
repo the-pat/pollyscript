afee0af9ccd82de0fbcbf33d7fdfb594a6050c6d
Patrick Tone <patrick@madronesoftware.com>
2020-09-25 12:04:09 +0200

    initialized project

---
+++ test/pollyscript.test.js
@@ -0,0 +1,9 @@
+const test = require('ava');
+
+const {hello} = require('../dist/pollyscript');
+
+test('`hello()` returns `Hello 🗺️!`', t => {
+  const result = hello();
+
+  t.is(result, `Hello 🗺️!`);
+});
